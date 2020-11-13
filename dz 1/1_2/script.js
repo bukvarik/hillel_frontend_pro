c295602363899066323cca86096748d354e6857d
Bukvarik <ihoriziumov@gmail.com>
2020-11-13 13:33:20 +0200

    1_2

---
+++ dz 1/1_2/script.js
@@ -0,0 +1,17 @@
+let a = +prompt('a', 0);
+let b = +prompt('b', 0);
+let c = +prompt('c', 0);
+
+let result = a + b + c;
+console.log(result);
+if (a % 2 == 0 ) {
+  console.log('a is even');
+} 
+
+if (b % 2 == 0) {
+  console.log('b is even');
+}
+
+if (c % 2 == 0) {
+  console.log('c is even');
+}
