ac8c523ab4a2e13c77f448869ece9bdcf3afe6c5
Bukvarik <ihoriziumov@gmail.com>
2020-11-19 19:16:34 +0200

    fixed i < n

--- dz 2/2_1/script.js
+++ dz 2/2_1/script.js
@@ -28,7 +28,7 @@ if (n < 2) {
   console.log(`Prime number`);
 } else {
   let count = 0;
-  for (let i = 2; i <= n; i++) {
+  for (let i = 2; i < n; i++) {
     if( n % i == 0) {
       count++;
     }
