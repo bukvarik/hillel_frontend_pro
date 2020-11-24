f398c4d95e4d7d3b15ef0c3b55613d503259ca39
Bukvarik <ihoriziumov@gmail.com>
2020-11-24 13:19:23 +0200

    homework 4_1

---
+++ dz 4/4_1/script.js
@@ -0,0 +1,14 @@
+let m = 50;
+let n = 50;
+center = Math.floor(m/2) + 1;
+
+for (let i = 1; i <= m; i++ ) {
+  for (let j = 1; j <= n; j++) {
+    if (i + j <= n + 1 && !(i <= j) || (i + j < n + 2) && j >= center && i <= center ) {
+      document.write(' 1 ');
+    } else {
+      document.write(' 0 ');
+    }
+  }
+  document.write('<br />');
+}
