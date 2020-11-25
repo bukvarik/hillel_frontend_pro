bceb2143f0d554af7879b3e296b628d256b81c94
Bukvarik <ihoriziumov@gmail.com>
2020-11-25 23:07:26 +0200

    completed homework 4_2

--- dz 4/4_2/script.js
+++ dz 4/4_2/script.js
@@ -1,25 +1,52 @@
 let m = 10;
 let n = m;
-
 A = new Array(n);
 
-for (let i = 0; i < A.length; i++) {
-  A[i] = new Array(m);
-  for (let j = 0; j < A[i].length; j++) {
-    A[i][j] = Math.floor(Math.random() * 40 - 20);
+for (let a = 0; a < A.length; a++) {
+  A[a] = new Array(m);
+  for (let b = 0; b < A[a].length; b++) {
+    A[a][b] = Math.floor(Math.random() * 40 - 20);
   }
-  document.write(A[i] + '<br />');
+}
+console.log(A);
+
+function rotate(matrix) {
+  const n = matrix.length;
+  const x = Math.floor(n/ 2);
+  const y = n - 1;
+  for (let i = 0; i < x; i++) {
+     for (let j = i; j < y - i; j++) {
+        temp = matrix[i][j];
+        matrix[i][j] = matrix[y - j][i];
+        matrix[y - j][i] = matrix[y - i][y - j];
+        matrix[y - i][y - j] = matrix[j][y - i]
+        matrix[j][y - i] = temp;
+     }
+  }
 }
 
-for (let k = 0; k < A.length; k++) {
-  let s = 0;
-  for (let l = 0; l < A.length; l++) {
-    s += A[l][k];
+console.log('rotated');
+rotate(A);
+console.log(A);
 
-    if ( s <= 0) {
-      document.write('*');
+function sum(arr) {
+  let s = 0;
+  for (let c = 0; c < arr.length; c++) {
+    for (let d = 0; d < arr[c].length; d++) {
+      s += arr[c][d];
+    } 
+    console.log(s);
+    if (s < 0) {
+      for (let g = 0; g < arr.length; g++) {
+        arr[c].shift();
+      }
     }
   }
-  
-  console.log(s);
+  console.log(A);
 }
+console.log('deleted s < 0');
+sum(A);
+rotate(A);
+rotate(A);
+rotate(A);
+console.log(A);
