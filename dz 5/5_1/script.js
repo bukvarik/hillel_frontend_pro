4a11a6f72f78c6eae5aaec7a6f25b0eeca0b1682
Bukvarik <ihoriziumov@gmail.com>
2020-11-28 23:45:33 +0200

    fixed 5_1

--- dz 5/5_1/script.js
+++ dz 5/5_1/script.js
@@ -2,8 +2,10 @@ let obj1 = {
   x: 1,
   y: 2,
   z: 3,
+  l: [1, 1, 1],
   text: 'Text',
-  text2: 'Text2'
+  text2: 'Text2',
+  t: [1, 1, 1, 1, 1, 1]
 }
 
 let obj2 = {
@@ -12,14 +14,14 @@ let obj2 = {
   o: 20,
   p: 23,
   bool1: true,
-  bool2: false
+  bool2: false,
+  j: [2, 3, 4, true, 'true']
 }
-
 allArrays = [];
 
 function addInArray(obj, array) {
   for (key in obj) {
-    if( typeof obj[key] == "string" || typeof obj[key] == "boolean") {
+    if( typeof obj[key] == "string" || typeof obj[key] == "boolean" || typeof obj[key] == 'number') {
       continue;
     }
     array.push(obj[key]);
@@ -32,8 +34,15 @@ console.log(allArrays);
 
 function sumInArray(arr) {
   let sum = 0
-  for( key in arr) {
-    sum += arr[key];
+  for (key in arr) {
+    //console.log(key);
+    for (keys in arr[key]) {
+      //console.log(arr[key][keys]);
+      if (typeof arr[key][keys] == 'string' || typeof arr[key][keys] == 'boolean') {
+        continue;
+      }
+      sum += arr[key][keys];
+    }
   }
   return sum;
 }
