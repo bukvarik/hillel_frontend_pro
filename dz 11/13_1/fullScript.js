588ccd61e367d8dd2bf42ac7af6781203387213e
Bukvarik <ihoriziumov@gmail.com>
2021-01-10 13:38:07 +0200

    homework 11

---
+++ dz 11/13_1/fullScript.js
@@ -0,0 +1,60 @@
+window.addEventListener('load', function() {
+    let box = document.querySelector('.box');
+    let moveBy = 50;
+    let jump = 100;
+
+    box.style.left = 0;
+    box.style.top = 0;
+
+    function handler(event) {
+        console.log('clicked', event, event.type);
+    }
+
+    function eventKeyDown(event) {
+        const key = event.key;
+        switch(key) {
+            case "w":
+            case "W":
+            case "ArrowUp":
+                box.style.top = parseInt(box.style.top) - moveBy + 'px';
+                console.log('Up');
+                break;
+            case "a":
+            case "A":
+            case "ArrowLeft":
+                box.style.left = parseInt(box.style.left) - moveBy + 'px';
+                console.log('Left');
+                break;
+            case "d":
+            case "D":
+            case "ArrowRight":
+                box.style.left = parseInt(box.style.left) + moveBy + 'px';
+                console.log('Right');
+                break;
+            case "s":
+            case "S":
+            case "ArrowDown":
+                box.style.top = parseInt(box.style.top) + moveBy + 'px';
+                console.log('Down');
+                break;
+            case " ":
+                box.style.top = parseInt(box.style.top) - jump + 'px';
+                setTimeout( function() {
+                    box.style.top = parseInt(box.style.top) + jump + 'px';
+                }, 2000);
+                console.log('Jump');
+                break;
+            case "Control":
+                box.style.transform = "matrix(2,0,0,.7,1,4)";
+                setTimeout( function() {
+                    box.style.transform = "matrix(1,0,0,1,0,0)";
+                }, 2000);
+                console.log('CTRL');
+                break;
+        }
+    }
+
+    document.addEventListener('keydown', eventKeyDown);
+});
+
+// Дополнительно: при зажатом CTRL персонаж может продолжать двигаться в ЛЕВО и ПРАВО! (т.е. ВВЕРХ и ВНИЗ не работают, ПРОБЕЛ не работает)
