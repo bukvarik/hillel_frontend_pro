dd48e23028a33acdcc4b4a1c1d32396eacde090f
Bukvarik <ihoriziumov@gmail.com>
2021-02-15 18:15:11 +0200

    fixed 17_1

--- dz 15/17_1/script.js
+++ dz 15/17_1/script.js
@@ -1,7 +1,4 @@
 let box = document.querySelector('.box');
-box.style.left = 0;
-box.style.top = 0;
-
 class Menu {
     #parrentEl = null;
     #element = null;
@@ -12,7 +9,7 @@ class Menu {
         this.#parrentEl = document.querySelector(selector);
         this.#element = document.createElement('button');
         this.#element.classList.add('menu__button');
-
+        
         this.#parrentEl.appendChild(this.#element);
 
         this.#content = content;
@@ -23,7 +20,7 @@ class Menu {
     }
 
     #addEvents() {
-        this.#element.addEventListener('click', this.#onClick) 
+        this.#element.addEventListener('click', this.#onClick);
     }
 
     render() {
@@ -32,25 +29,99 @@ class Menu {
 }
 
 window.addEventListener('load', () => {
+    let moveBy = 50;
+    let jumpY = 100;
+
+    box.style.left = 0;
+    box.style.top = 0;
+
+    function handler(event) {
+        console.log('clicked', event, event.type);
+    }
+
+    let throttle = false;
+
+    function eventKeyDown(event) {
+        const key = event.key;
+        switch(key) {
+            case "w":
+            case "W":
+            case "ArrowUp":
+                box.style.top = parseInt(box.style.top) - moveBy + 'px';
+                console.log('Up');
+                break;
+            case "a":
+            case "A":
+            case "ArrowLeft":
+                box.style.left = parseInt(box.style.left) - moveBy + 'px';
+                console.log('Left');
+                break;
+            case "d":
+            case "D":
+            case "ArrowRight":
+                box.style.left = parseInt(box.style.left) + moveBy + 'px';
+                console.log('Right');
+                break;
+            case "s":
+            case "S":
+            case "ArrowDown":
+                box.style.top = parseInt(box.style.top) + moveBy + 'px';
+                console.log('Down');
+                break;
+            case " ":
+                if (throttle == true) {
+                    return true;
+                }
+        
+                throttle = true;    
+                setTimeout(() => {
+                    throttle = false;
+                }, 2000);
+                
+                box.style.top = parseInt(box.style.top) - jumpY + 'px';
+                setTimeout( function() {
+                    box.style.top = parseInt(box.style.top) + jumpY + 'px';
+                }, 2000);
+                console.log('Jump');
+                break;
+            case "Control":
+                box.style.transform = "matrix(2,0,0,.7,1,4)";
+                setTimeout( function() {
+                    box.style.transform = "matrix(1,0,0,1,0,0)";
+                }, 2000);
+                console.log('CTRL');
+                break;
+            
+        }
+    }
+
+    document.addEventListener('click', () => {
+        hideContextMenu();
+    });
+
+    document.addEventListener('keydown', eventKeyDown);
     //global menu
+    // debugger;
     const add = new Menu({
-        selector: '#globalMenu',
+        selector: '.global',
         onClick: () => {
             document.body.appendChild(box);
         },
-        content: 'add'
+        content: 'add',
     });
+    console.log(add);
 
     const remove = new Menu({
-        selector: '#globalMenu',
+        selector: '.global',
         onClick: () => {
             box.remove();
         },
         content: 'remove'
     });
+    console.log(remove);
 
     const resetPosition = new Menu({
-        selector: '#globalMenu',
+        selector: '.global',
         onClick: () => {
             box.style.left = 0,
             box.style.top = 0
@@ -60,12 +131,21 @@ window.addEventListener('load', () => {
 
     //box menu
     const jump = new Menu({
-        selector: '#context-menu',
+        selector: '.onBox',
         onClick: () => {
 
-            box.style.top = parseInt(box.style.top) - 100 + 'px';
+            if (throttle == true) {
+                return true;
+            }
+    
+            throttle = true; 
+            setTimeout(() => {
+                throttle = false;
+            }, 2000);   
+
+            box.style.top = parseInt(box.style.top) - jumpY + 'px';
             setTimeout( function() {
-                box.style.top = parseInt(box.style.top) + 100 + 'px';
+                box.style.top = parseInt(box.style.top) + jumpY + 'px';
             }, 2000);
             console.log('Jump');
         },
@@ -73,7 +153,7 @@ window.addEventListener('load', () => {
     });
     
     const moveLeft = new Menu({
-        selector: '#context-menu',
+        selector: '.onBox',
         onClick: () => {
             box.style.left = parseInt(box.style.left) - 50 + 'px';
         },
@@ -81,21 +161,21 @@ window.addEventListener('load', () => {
         
     });
     const moveRight = new Menu({
-        selector: '#context-menu',
+        selector: '.onBox',
         onClick: () => {
             box.style.left = parseInt(box.style.left) + 50 + 'px';
         },
         content: 'Move Right'
     });
     const moveTop = new Menu({
-        selector: '#context-menu',
+        selector: '.onBox',
         onClick: () => {
             box.style.top = parseInt(box.style.top) - 50 + 'px';
         },
         content: 'Move Top'
     });
     const moveDown = new Menu({
-        selector: '#context-menu',
+        selector: '.onBox',
         onClick: () => {
             box.style.top = parseInt(box.style.top) + 50 + 'px';
         },
@@ -106,24 +186,28 @@ window.addEventListener('load', () => {
 // box menu open 
 box.addEventListener('contextmenu', (e) => {
     e.preventDefault();
-    let contextMenu = document.querySelector('#context-menu');
-    contextMenu.style.top = e.offsetY + 'px';
-    contextMenu.style.left = e.offsetX + 'px';
+    e.stopPropagation();
+    let contextMenu = document.querySelector('.context-menu.onBox');
+    contextMenu.style.top = e.clientY + 'px';
+    contextMenu.style.left = e.clientX + 'px';
+    hideContextMenu();
+
     contextMenu.classList.add('active');
 })
 //global menu close
 window.addEventListener('contextmenu', (e) => {
-    e.preventDefault();
-    let contextMenu = document.querySelector('#globalMenu');
-    contextMenu.style.top = e.offsetY + 'px';
-    contextMenu.style.left = e.offsetX + 'px';
+    e.preventDefault(); 
+    let contextMenu = document.querySelector('.context-menu.global');
+    contextMenu.style.top = e.clientY + 'px';
+    contextMenu.style.left = e.clientX + 'px';
+    hideContextMenu();
+
     contextMenu.classList.add('active');
 });
-// box menu close
-window.addEventListener('click', function() {
-    document.querySelector('#context-menu').classList.remove('active');
-});
-//global menu close
-window.addEventListener('click', () => {
-    document.querySelector('#globalMenu').classList.remove('active');
-});
+
+function hideContextMenu() {
+    const contextMenus = document.querySelectorAll('.context-menu');
+    contextMenus.forEach(menu => {
+        menu.classList.remove('active');
+    });
+}
